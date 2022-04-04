b010996788939ec6f6668ab91f59482d19c054fd
priks <gautamprikshit1@gmail.com>
2022-04-04 16:30:55 +0530

    Initial commit

--- entity/ball.go
+++ entity/ball.go
@@ -36,11 +36,31 @@ func (b *Ball) Update(leftPaddle *Paddle, rightPaddle *Paddle) {
 	if b.X-b.Radius < leftPaddle.X+float32(leftPaddle.Width/2) &&
 		b.Y > leftPaddle.Y-float32(leftPaddle.Height/2) &&
 		b.Y < leftPaddle.Y+float32(leftPaddle.Height/2) {
+		if b.XVelocity < 0 {
+			b.XVelocity -= 0.0001
+		} else {
+			b.XVelocity += 0.0001
+		}
+		if b.YVelocity < 0 {
+			b.YVelocity -= 0.0001
+		} else {
+			b.YVelocity += 0.0001
+		}
 		b.XVelocity = -b.XVelocity
 		b.X = leftPaddle.X + float32(leftPaddle.Width/2) + b.Radius
 	} else if b.X+b.Radius > rightPaddle.X-float32(rightPaddle.Width/2) &&
 		b.Y > rightPaddle.Y-float32(rightPaddle.Height/2) &&
 		b.Y < rightPaddle.Y+float32(rightPaddle.Height/2) {
+		if b.XVelocity < 0 {
+			b.XVelocity -= 0.0001
+		} else {
+			b.XVelocity += 0.0001
+		}
+		if b.YVelocity < 0 {
+			b.YVelocity -= 0.0001
+		} else {
+			b.YVelocity += 0.0001
+		}
 		b.XVelocity = -b.XVelocity
 		b.X = rightPaddle.X - float32(rightPaddle.Width/2) - b.Radius
 	}
